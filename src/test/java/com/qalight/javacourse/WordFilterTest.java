43c94297a6489992f31b16b1059be1758a255d61
stkotok <stkotok@gmail.com>
2014-07-17 17:35:24 +0300

    WordFilterTest created

---
+++ src/test/java/com/qalight/javacourse/WordFilterTest.java
@@ -0,0 +1,16 @@
+package com.qalight.javacourse;
+
+import org.junit.Test;
+
+public class WordFilterTest {
+
+    @Test
+    public void testFilterWord() throws Exception {
+        System.out.println("1");
+    }
+
+    @Test
+    public void testParseUrlForDb() throws Exception {
+        System.out.println("2");
+    }
+}
