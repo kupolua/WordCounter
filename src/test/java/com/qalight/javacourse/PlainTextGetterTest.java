57d2552cff4d95a3331767d48ffd436504a4bd51
stkotok <stkotok@gmail.com>
2014-07-17 16:29:02 +0300

    PlainTextGetterTest created

---
+++ src/test/java/com/qalight/javacourse/PlainTextGetterTest.java
@@ -0,0 +1,24 @@
+package com.qalight.javacourse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlainTextGetterTest {
+
+    @Test
+    public void testGetPlainTextByUrl() throws Exception {
+
+        // given
+        final String testUrl = "english-e-books.net/books/advanced/Charlie_and_the_Chocolate_Factory-Dahl_Roald/Charlie_and_the_Chocolate_Factory-Dahl_Roald.txt";
+        final int expectedTextSize = 156832;
+
+        // when
+        PlainTextGetter plainTextGetter = new PlainTextGetter();
+        String actualText = plainTextGetter.getPlainTextByUrl(testUrl);
+
+        // then
+        Assert.assertSame(testUrl.getClass(), actualText.getClass());
+        Assert.assertEquals(expectedTextSize, actualText.length());
+
+    }
+}
