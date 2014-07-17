b923443198b4f8a7161f39e6f65e47bc2d971fc6
stkotok <stkotok@gmail.com>
2014-07-17 16:29:56 +0300

    WordCounterTest created

---
+++ src/test/java/com/qalight/javacourse/WordCounterTest.java
@@ -0,0 +1,30 @@
+package com.qalight.javacourse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounterTest {
+
+    @Test
+    public void testCountWords() throws Exception {
+
+        // given
+        final String text = "one , : \" ? \n \t ; \\ / * two, two, three, three, three.";
+        final Map<String, Integer> expectedResult = new HashMap<String, Integer>(){{
+            put("one", 1);
+            put("two", 2);
+            put("three", 3);
+        }};
+
+        // when
+        WordCounter wordCounter = new WordCounter();
+        final Map<String, Integer> actualResult = wordCounter.countWords(text);
+
+        // then
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+}
