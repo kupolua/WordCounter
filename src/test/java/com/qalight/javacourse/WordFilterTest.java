1c357e17fe48c17d59643581ba3cb3f4cee4448d
kupol <pkulakovsky@gmail.com>
2014-07-17 19:23:08 +0300

    add: kpl-oo check pointuu

--- src/test/java/com/qalight/javacourse/WordFilterTest.java
+++ src/test/java/com/qalight/javacourse/WordFilterTest.java
@@ -6,11 +6,11 @@ public class WordFilterTest {
 
     @Test
     public void testFilterWord() throws Exception {
-        System.out.println("11!33!-kpl");
+        System.out.println("11!33!-kplt");
     }
 
     @Test
     public void testParseUrlForDb() throws Exception {
-        System.out.println("22!44!-kpl");
+        System.out.println("22!44!-kplt");
     }
 }
