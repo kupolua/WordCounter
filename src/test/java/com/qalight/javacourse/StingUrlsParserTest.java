9266158316b3953de063621fdb93ab03fc648d99
stkotok <stkotok@gmail.com>
2014-07-19 17:09:13 +0300

    WORDS-21 Create new class and test to parse String Urls from WebForm. StingUrlsParser

---
+++ src/test/java/com/qalight/javacourse/StingUrlsParserTest.java
@@ -0,0 +1,29 @@
+package com.qalight.javacourse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StingUrlsParserTest {
+
+    @Test
+    public void testUrlList() throws Exception {
+
+        // given
+        final String userPostStringUrls = "http://htmlbook.ru/html/input, http://stackoverflow.com/questions/2349633/servlets-doget-and-dopost, http://www.gradle.org/docs/current/release-notes";
+        final List<String> expectedList = new ArrayList<String>() {{
+            add("http://htmlbook.ru/html/input");
+            add("http://stackoverflow.com/questions/2349633/servlets-doget-and-dopost");
+            add("http://www.gradle.org/docs/current/release-notes");
+        }};
+
+        //when
+        List<String> actualList = new StingUrlsParser().urlList(userPostStringUrls);
+
+        //then
+        Assert.assertEquals(expectedList, actualList);
+
+    }
+}
