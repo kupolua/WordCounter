4362ac312eead1cc5b296dd2a2d3d9dc35df716e
diverfd <diverfd@gmail.com>
2014-07-30 17:22:25 +0300

    WORDS-56 todo: give full log message UserHTMLFormLoader

--- src/main/java/com/qalight/javacourse/servlet/UserHTMLFormLoader.java
+++ src/main/java/com/qalight/javacourse/servlet/UserHTMLFormLoader.java
@@ -1,6 +1,8 @@
 package com.qalight.javacourse.servlet;
 
 import com.qalight.javacourse.ReadFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,23 +15,23 @@ import java.io.IOException;
  */
 // todo: format code
 public class UserHTMLFormLoader extends HttpServlet {
-
+    private static final Logger LOG = LoggerFactory.getLogger(UserHTMLFormLoader.class);
     private static final long serialVersionUID = -6154475799000019575L;
 
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String userHTMLForm;
 
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
 
-        String message;
-        try {
-            message = userHTMLFormLoader("index.html");
-        } catch (Exception e) {
-            message = "Дорогой клиент, у тябя вот такая ошибка: " + e.getMessage();
-        }
+        userHTMLForm = userHTMLFormLoader("index.html");
 
-        response.getWriter().println(message);
+        try {
+            LOG.info("Try to print user HTML form.");
+            response.getWriter().println(userHTMLForm);
+        } catch (IOException e) {
+            LOG.error("userHTMLForm can't be printed.", e);
+        }
     }
 
     public void doPost(HttpServletRequest request,
