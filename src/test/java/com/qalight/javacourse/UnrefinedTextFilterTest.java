8c8b4d063e4261d2d95a2cb8ad29969c2ee699d3
diverfd <diverfd@gmail.com>
2014-08-07 11:58:15 +0300

    WORDS-68 create UnrefinedTextFilter test

---
+++ src/test/java/com/qalight/javacourse/UnrefinedTextFilterTest.java
@@ -0,0 +1,22 @@
+package com.qalight.javacourse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnrefinedTextFilterTest {
+
+    @Test
+    public void testRefineText() throws Exception {
+        UnrefinedTextFilter textFilter = new UnrefinedTextFilter();
+        //given
+        final String unrefinedWord = "(Play;Station)";
+        //when
+        String actualResult = textFilter.refineText(unrefinedWord);
+        //then
+        final String expectedResult = " play station ";
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
