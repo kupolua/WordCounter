5c854790851079e2821dfae33581ed446076c5f4
StKotok <stkotok@gmail.com>
2014-08-08 20:10:01 -0700

    WORDS-104 - HtmlToStringConverterTest refactoring - test actual text, not size

--- src/test/java/com/qalight/javacourse/HtmlToStringConverterTest.java
+++ src/test/java/com/qalight/javacourse/HtmlToStringConverterTest.java
@@ -1,22 +1,25 @@
 package com.qalight.javacourse;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 public class HtmlToStringConverterTest {
 
     @Test
     // todo: remove 'throws Exception everywhere where not necessary'
-    // tod: move to integration test. unit tests should not depend on internet connection
+    // todo: move to integration test. unit tests should not depend on internet connection
     public void testGetPlainTextByUrl() throws Exception {
 
         //   given
         final String testUrl = "http://www.mono-project.com/Java";
-        final String expectedResult = new String(
-                Files.readAllBytes(Paths.get("src\\test\\resources\\expectedResultForHtmlToStringConverterTest.txt")));
+        InputStream in;
+        in = this.getClass().getClassLoader().getResourceAsStream("expectedResultForHtmlToStringConverterTest.txt");
+        final String expectedResult = IOUtils.toString(in, "UTF-8");
+//        final String expectedResult = new String(
+//                Files.readAllBytes(Paths.get("./src/test/resources/expectedResultForHtmlToStringConverterTest.txt")));
 
         // when
         ToStringConverter HTMLToTextConverter = new HtmlToStringConverter();
