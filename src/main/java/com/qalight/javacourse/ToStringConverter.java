4eff923876177a6d66887278837076652b2cf1cc
kupol <pkulakovsky@gmail.com>
2014-08-11 19:30:59 +0300

    Clean all files for new architecture

--- src/main/java/com/qalight/javacourse/ToStringConverter.java
+++
@@ -1,9 +0,0 @@
-package com.qalight.javacourse;
-
-/**
- * Created by emix on 8/6/14.
- */
-public interface ToStringConverter {
-
-    String convertToString (String param);
-}
--- src/main/java/com/qalight/javacourse/service/ResultType.java
+++
@@ -1,8 +0,0 @@
-package com.qalight.javacourse.service;
-
-/**
- * Created by emix on 8/9/14.
- */
-public interface ResultType {
-
-}
