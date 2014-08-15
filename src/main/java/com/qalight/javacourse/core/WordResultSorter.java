7fc811d4e3312e7f5b02a2ed930bc2414f43a6f7
StKotok <stkotok@gmail.com>
2014-08-15 16:42:40 +0300

    WordResultSorter: 1) anonymous Comparator replaced by Lambda, 2) delete comments

--- src/main/java/com/qalight/javacourse/core/WordResultSorter.java
+++ src/main/java/com/qalight/javacourse/core/WordResultSorter.java
@@ -9,22 +9,16 @@ public enum WordResultSorter {
         public List<Map.Entry<String, Integer>> getSortedWords(Map<String, Integer> countedWords) {
             Set<Map.Entry<String, Integer>> set = countedWords.entrySet();
             List<Map.Entry<String, Integer>> sortedWords = new ArrayList<Map.Entry<String, Integer>>(set);
-            Collections.sort(sortedWords, new Comparator<Map.Entry<String, Integer>>() {
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-
-                    boolean isMapData01Empty = (o1.getKey() == null || o1.getKey().equals(""));
-                    boolean isMapData02Empty = (o2.getKey() == null || o2.getKey().equals(""));
-
-                    if (isMapData01Empty && isMapData02Empty)
-                        return 0;
-                    // at least one of them is not empty
-                    if (isMapData01Empty)
-                        return -1;
-                    if (isMapData02Empty)
-                        return 1;
-                    //none of them is empty
-                    return (o1.getKey()).compareTo(o2.getKey());
-                }
+            Collections.sort(sortedWords, (o1, o2) -> {
+                boolean isMapData01Empty = (o1.getKey() == null || o1.getKey().equals(""));
+                boolean isMapData02Empty = (o2.getKey() == null || o2.getKey().equals(""));
+                if (isMapData01Empty && isMapData02Empty)
+                    return 0;
+                if (isMapData01Empty)
+                    return -1;
+                if (isMapData02Empty)
+                    return 1;
+                return (o1.getKey()).compareTo(o2.getKey());
             });
             return sortedWords;
         }
@@ -35,22 +29,16 @@ public enum WordResultSorter {
         public List<Map.Entry<String, Integer>> getSortedWords(Map<String, Integer> countedWords) {
             Set<Map.Entry<String, Integer>> set = countedWords.entrySet();
             List<Map.Entry<String, Integer>> sortedWords = new ArrayList<Map.Entry<String, Integer>>(set);
-            Collections.sort(sortedWords, new Comparator<Map.Entry<String, Integer>>() {
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-
-                    boolean isMapData01Empty = (o1.getKey() == null || o1.getKey().equals(""));
-                    boolean isMapData02Empty = (o2.getKey() == null || o2.getKey().equals(""));
-
-                    if (isMapData01Empty && isMapData02Empty)
-                        return 0;
-                    // at least one of them is not empty
-                    if (isMapData01Empty)
-                        return -1;
-                    if (isMapData02Empty)
-                        return 1;
-                    //none of them is empty
-                    return (o2.getKey()).compareTo(o1.getKey());
-                }
+            Collections.sort(sortedWords, (o1, o2) -> {
+                boolean isMapData01Empty = (o1.getKey() == null || o1.getKey().equals(""));
+                boolean isMapData02Empty = (o2.getKey() == null || o2.getKey().equals(""));
+                if (isMapData01Empty && isMapData02Empty)
+                    return 0;
+                if (isMapData01Empty)
+                    return -1;
+                if (isMapData02Empty)
+                    return 1;
+                return (o2.getKey()).compareTo(o1.getKey());
             });
             return sortedWords;
         }
@@ -62,22 +50,16 @@ public enum WordResultSorter {
             Set<Map.Entry<String, Integer>> set = countedWords.entrySet();
             List<Map.Entry<String, Integer>> sortedWords = new ArrayList<Map.Entry<String, Integer>>(set);
             // todo stkotok: use comparator classes instead of anonymous comparators
-            Collections.sort(sortedWords, new Comparator<Map.Entry<String, Integer>>() {
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-
-                    boolean isMapData01Empty = (o1.getValue() == null || o1.getValue().equals(""));
-                    boolean isMapData02Empty = (o2.getValue() == null || o2.getValue().equals(""));
-
-                    if (isMapData01Empty && isMapData02Empty)
-                        return 0;
-                    // at least one of them is not empty
-                    if (isMapData01Empty)
-                        return -1;
-                    if (isMapData02Empty)
-                        return 1;
-                    //none of them is empty
-                    return (o1.getValue()).compareTo(o2.getValue());
-                }
+            Collections.sort(sortedWords, (o1, o2) -> {
+                boolean isMapData01Empty = (o1.getValue() == null || o1.getValue().equals(""));
+                boolean isMapData02Empty = (o2.getValue() == null || o2.getValue().equals(""));
+                if (isMapData01Empty && isMapData02Empty)
+                    return 0;
+                if (isMapData01Empty)
+                    return -1;
+                if (isMapData02Empty)
+                    return 1;
+                return (o1.getValue()).compareTo(o2.getValue());
             });
             return sortedWords;
         }
@@ -88,28 +70,21 @@ public enum WordResultSorter {
         public List<Map.Entry<String, Integer>> getSortedWords(Map<String, Integer> countedWords) {
             Set<Map.Entry<String, Integer>> set = countedWords.entrySet();
             List<Map.Entry<String, Integer>> sortedWords = new ArrayList<Map.Entry<String, Integer>>(set);
-            Collections.sort(sortedWords, new Comparator<Map.Entry<String, Integer>>() {
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-
-                    boolean isMapData01Empty = (o1.getValue() == null || o1.getValue().equals(""));
-                    boolean isMapData02Empty = (o2.getValue() == null || o2.getValue().equals(""));
-
-                    if (isMapData01Empty && isMapData02Empty)
-                        return 0;
-                    // at least one of them is not empty
-                    if (isMapData01Empty)
-                        return -1;
-                    if (isMapData02Empty)
-                        return 1;
-                    //none of them is empty
-                    return (o2.getValue()).compareTo(o1.getValue());
-                }
+            Collections.sort(sortedWords, (o1, o2) -> {
+                boolean isMapData01Empty = (o1.getValue() == null || o1.getValue().equals(""));
+                boolean isMapData02Empty = (o2.getValue() == null || o2.getValue().equals(""));
+                if (isMapData01Empty && isMapData02Empty)
+                    return 0;
+                if (isMapData01Empty)
+                    return -1;
+                if (isMapData02Empty)
+                    return 1;
+                return (o2.getValue()).compareTo(o1.getValue());
             });
             return sortedWords;
         }
     };
 
     public abstract List<Map.Entry<String, Integer>> getSortedWords(Map<String, Integer> countedWords);
-
 
 }
