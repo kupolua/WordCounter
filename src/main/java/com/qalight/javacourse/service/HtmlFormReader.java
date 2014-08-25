885b64d569414abe48e5bdf12cd27aeab69e4af4
Pavel Kulakovsky <1215@i.ua>
2014-08-25 16:10:57 +0300

    rename integTest to integrationTest

--- src/main/java/com/qalight/javacourse/service/HtmlFormReader.java
+++ src/main/java/com/qalight/javacourse/service/HtmlFormReader.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 
 public class HtmlFormReader {
     private static final Logger LOG = LoggerFactory.getLogger(HtmlFormReader.class);
-    private static final String NO_HTML_SOURCE_FILE = "No Source File ";
+    private static final String NO_HTML_SOURCE_FILE = "No Source File";
     private static final String ERROR_WEB_PAGE = "error.html";
 
     public String readHtmlSourceFile(String fileName) {
