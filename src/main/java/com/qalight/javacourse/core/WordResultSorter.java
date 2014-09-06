98e7c5e6990768af304700f3660ea16549e186cf
StKotok <stkotok@gmail.com>
2014-09-06 11:14:17 +0300

    WordResultSorter: use Java 8 approach

--- src/main/java/com/qalight/javacourse/core/WordResultSorter.java
+++ src/main/java/com/qalight/javacourse/core/WordResultSorter.java
@@ -4,6 +4,58 @@ import java.util.*;
 
 public enum WordResultSorter {
     // todo: use Java 8 approach
+//    KEY_ASCENDING {
+//        @Override
+//        public Map<String, Integer> getSortedWords(Map<String, Integer> countedWords) {
+//            Map<String, Integer> sortedWords = new TreeMap<String, Integer>(countedWords);
+//            return sortedWords;
+//        }
+//    },
+//
+//    KEY_DESCENDING {
+//        @Override
+//        public Map<String, Integer> getSortedWords(Map<String, Integer> countedWords) {
+//            Map<String, Integer> sortedWords = new TreeMap<String, Integer>(Collections.reverseOrder());
+//            sortedWords.putAll(countedWords);
+//            return sortedWords;
+//        }
+//    },
+//
+//    VALUE_ASCENDING {
+//        @Override
+//        public Map<String, Integer> getSortedWords(Map<String, Integer> countedWords) {
+//            List list = new LinkedList<>(countedWords.entrySet());
+//            Collections.sort(list, (Object o1, Object o2) -> {
+//                return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+//            });
+//
+//            Map sortedWords = new LinkedHashMap();
+//            for (Object aList : list) {
+//                Map.Entry entry = (Map.Entry) aList;
+//                sortedWords.put(entry.getKey(), entry.getValue());
+//            }
+//            return sortedWords;
+//        }
+//    },
+//
+//    VALUE_DESCENDING {
+//        @Override
+//        public Map<String, Integer> getSortedWords(Map<String, Integer> countedWords) {
+//            List list = new LinkedList<>(countedWords.entrySet());
+//            Collections.sort(list, (o2, o1) -> {
+//                return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+//            });
+//
+//            Map sortedWords = new LinkedHashMap();
+//            for (Object aList : list) {
+//                Map.Entry entry = (Map.Entry) aList;
+//                sortedWords.put(entry.getKey(), entry.getValue());
+//            }
+//            return sortedWords;
+//        }
+//    };
+//
+//    public abstract Map<String, Integer> getSortedWords(Map<String, Integer> countedWords);
 
     KEY_ASCENDING {
         @Override
