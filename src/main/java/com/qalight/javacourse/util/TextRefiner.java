6867bd54be600673bf4eb70bfa24b39a6c827d7e
StKotok <stkotok@gmail.com>
2014-09-08 17:07:06 +0300

    new TextRefiner

--- src/main/java/com/qalight/javacourse/util/TextRefiner.java
+++ src/main/java/com/qalight/javacourse/util/TextRefiner.java
@@ -4,64 +4,75 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class TextRefiner {
     private static final Logger LOG = LoggerFactory.getLogger(TextRefiner.class);
-    private static final String WHITESPACES_MATCHER = "\\s+";
-    //    private static final String NON_BREAKING_HYPHEN = "[999]";
+    private static final String WHITESPACES_MATCHER =
+            "(\\s+)|(&nbsp;)|(&#160;)|(&ensp;)|(&#8194;)|(&emsp;)|(&#8195;)|(&thinsp;)|(&#8201;)|(&zwnj;)|(&#8204;)|—";
     private static final String NON_BREAKING_HYPHEN = "&#8";
-    private static final String DASH = "—";
-    private static final Pattern NON_WORD_LETTER_PATTERN = Pattern.compile("[^a-zA-Zа-яА-Я-іІїЇєЄёЁґҐ]");
-//    private static final Pattern WHITESPACES_PATTERN = Pattern.compile("[^a-zA-Zа-яА-Я-іІїЇєЄёЁґҐ]");
+    private static final String URL_PATTERN = "";
+    private static final String EMAIL_PATTERN = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$";
+    private static final Pattern CLEAN_PATTERN = Pattern.compile("[^a-zA-Zа-яА-Я-іІїЇєЄёЁґҐ]");
 
     public List<String> refineText(String unrefinedPlainText) {
 
         checkIfPlainTextIsNullOrEmpty(unrefinedPlainText);
 
-        String[] splitWords = unrefinedPlainText.split(WHITESPACES_MATCHER);
-        List<String> words = new ArrayList<>();
-        Collections.addAll(words, splitWords);
+        List<String> words = new ArrayList<>(Arrays.asList(unrefinedPlainText.split(WHITESPACES_MATCHER)));
 
-        List<String> wordsWithoutDash = splitByDash(words);
+        System.out.println(" Splitted text: " + words);
 
-        List<String> wordsWithNormalHyphen = replaceSpecSymbols(wordsWithoutDash);
+        List<List<String>> listList = separateEmailsAndUrls(words);
 
-        return wordsWithNormalHyphen;
-    }
+        words = replaceSpecSymbols(listList.get(0));
 
-    private List<String> splitByDash(List<String> words) {
-        List<String> allDashedWordsToClean = new ArrayList<>();
-        List<String> allSeparatedWordsToAdd = new ArrayList<>();
-        for (String potentialDashedWord : words) {
-            if (potentialDashedWord.contains(DASH)) {
-                String[] separatedWords = potentialDashedWord.split(DASH);
-                allDashedWordsToClean.add(potentialDashedWord);
-                Collections.addAll(allSeparatedWordsToAdd, separatedWords);
-            }
-        }
-        words.removeAll(allDashedWordsToClean);
-        words.addAll(allSeparatedWordsToAdd);
+        words = cleanWords(words);
+
         return words;
     }
 
-    public List<String> replaceSpecSymbols(List<String> words) {
+    public List<List<String>> separateEmailsAndUrls(List<String> words) {
+        List<String> emailsAdnUrls = new ArrayList<>();
+        for (String word : words) {
+            if (word.contains(EMAIL_PATTERN)) {
+                emailsAdnUrls.add(word.trim());
+                words.remove(word);
+            }
+        }
+        List<List<String>> listList = new ArrayList<>();
+        listList.add(words);
+        listList.add(emailsAdnUrls);
+        return listList;
+    }
+
+    private List<String> replaceSpecSymbols(List<String> words) {
         for (int i = 0; i < words.size(); i++) {
             String word = words.get(i);
             if (word.contains(NON_BREAKING_HYPHEN)) {
-                System.out.println("\tunhandled word = " + word);
                 word = word.replaceAll(NON_BREAKING_HYPHEN, "-");
-                System.out.println("\t  handled word = " + word);
                 words.set(i, word);
             }
         }
         return words;
     }
 
-    private String refineWord(String dirtyWord) {
-        return NON_WORD_LETTER_PATTERN.matcher(dirtyWord).replaceAll("").toLowerCase();
+    private List<String> cleanWords(List<String> words) {
+        int i = 0;
+        while (i < words.size()) {
+            String word = words.get(i);
+            word = CLEAN_PATTERN.matcher(word).replaceAll("").toLowerCase();
+            if (word.trim().isEmpty()) {
+                words.remove(i);
+            } else {
+                words.set(i, word);
+                i++;
+            }
+        }
+        return words;
     }
 
     private void checkIfPlainTextIsNullOrEmpty(String unrefinedPlainText) {
--- src/test/java/com/qalight/javacourse/util/TextRefinerTest.java
+++ src/test/java/com/qalight/javacourse/util/TextRefinerTest.java
@@ -34,7 +34,7 @@ public class TextRefinerTest {
     @Test
     public void testGetRefinedTextTEST() {
         //given
-        final String givenText = "[One—one&#160; ONE-oNE  Two&#8two, two!, three, three—усіх]";
+        final String givenText = "[stkotok@gmail.com [One—one&#160; ONE-oNE  Two&#8two, two!, three, three—усіх]";
         System.out.println("Unrefined text: " + givenText);
 
         //when
@@ -42,8 +42,23 @@ public class TextRefinerTest {
         System.out.println("  Refined text: " + actual.toString());
 
         //then
-        List<String> expected = Arrays.asList("one", "one", "one-one", "two", "two", "two", "three", "three", "усіх");
-//        Assert.assertEquals(expected, actual);
+        String expected = "[one, one, one-one, two-two, two, three, three, усіх]";
+//        Assert.assertEquals(expected, actual.toString());
+    }
+
+    @Test
+    public void testEmail() {
+        //given
+        final String givenText = "stkotok@gmail.com ";
+        System.out.println("Unrefined text: " + givenText);
+
+        //when
+        List<String> actual = refiner.refineText(givenText);
+        System.out.println("  Refined text: " + actual.toString());
+
+        //then
+        String expected = "[one, one, one-one, two-two, two, three, three, усіх]";
+//        Assert.assertEquals(expected, actual.toString());
     }
 
     @Test(expected = IllegalArgumentException.class)
