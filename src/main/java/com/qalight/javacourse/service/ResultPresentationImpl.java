c0931e6fd96d08ce103f287061c30aaf7df90adc
StKotok <stkotok@gmail.com>
2014-09-08 13:42:37 +0300

    develop restore

--- src/main/java/com/qalight/javacourse/service/ResultPresentationImpl.java
+++ src/main/java/com/qalight/javacourse/service/ResultPresentationImpl.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by pavelkulakovsky on 27.08.14.
- */
 @Component
 public class ResultPresentationImpl {
     private static final Logger LOG = LoggerFactory.getLogger(ResultPresentationImpl.class);
