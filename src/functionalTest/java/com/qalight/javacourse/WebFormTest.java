a54e097a3c4f87496466713c589140608b8f50b7
vkamenniy <vkamenniy@gmail.com>
2014-09-12 15:28:49 +0300

    WORDS-188 - WebFormTest.java (32, 7) //todo: add test for search word and Show entries and Previous, Next page, input text

--- src/functionalTest/java/com/qalight/javacourse/WebFormTest.java
+++ src/functionalTest/java/com/qalight/javacourse/WebFormTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -131,6 +132,93 @@ public class WebFormTest {
         assertEquals(EXPECTED_SORTING_VALUE_DESCENDING, actualSortingValueDescending);
     }
 
+    @Test
+    public void testSearchWord() throws Exception {
+        // given
+        driver.get(baseUrl);
+
+        // when
+        driver.findElement(By.id("userUrlsList")).clear();
+        driver.findElement(By.id("userUrlsList")).sendKeys(HTML_TEST_PAGE);
+        driver.findElement(By.id("button")).click();
+        driver.findElement(By.cssSelector("input[type=\"search\"]")).clear();
+        driver.findElement(By.cssSelector("input[type=\"search\"]")).sendKeys("білка");
+        sleep(TIME_WAIT);
+
+        // then
+        String actualSearchWord = driver.findElement(By.id("ajaxResponse")).getText();
+        assertEquals(EXPECTED_SEARCH_WORD, actualSearchWord);
+    }
+
+    @Test
+    public void testNextResponse() throws Exception {
+        // given
+        driver.get(baseUrl);
+
+        // when
+        driver.findElement(By.id("userUrlsList")).clear();
+        driver.findElement(By.id("userUrlsList")).sendKeys(HTML_TEST_PAGE);
+        driver.findElement(By.id("button")).click();
+        sleep(TIME_WAIT);
+        driver.findElement(By.linkText("Next")).click();
+
+        // then
+        String actualNextResponse = driver.findElement(By.id("ajaxResponse")).getText();
+        assertEquals(EXPECTED_NEXT_RESPONSE, actualNextResponse);
+    }
+
+    @Test
+    public void testPreviousResponse() throws Exception {
+        // given
+        driver.get(baseUrl);
+
+        // when
+        driver.findElement(By.id("userUrlsList")).clear();
+        driver.findElement(By.id("userUrlsList")).sendKeys(HTML_TEST_PAGE);
+        driver.findElement(By.id("button")).click();
+        sleep(TIME_WAIT);
+        driver.findElement(By.linkText("Next")).click();
+        driver.findElement(By.linkText("Previous")).click();
+
+
+        // then
+        String actualPreviousResponse = driver.findElement(By.id("ajaxResponse")).getText();
+        assertEquals(EXPECTED_PREVIOUS_RESPONSE, actualPreviousResponse);
+    }
+
+    @Test
+    public void testShowEntries() throws Exception {
+        // given
+        driver.get(baseUrl);
+
+        // when
+        driver.findElement(By.id("userUrlsList")).clear();
+        driver.findElement(By.id("userUrlsList")).sendKeys(HTML_TEST_PAGE);
+        driver.findElement(By.id("button")).click();
+        sleep(TIME_WAIT);
+        new Select(driver.findElement(By.name("example_length"))).selectByVisibleText("25");
+
+        // then
+        String actualShowEntries = driver.findElement(By.id("ajaxResponse")).getText();
+        assertEquals(EXPECTED_SHOW_ENTRIES, actualShowEntries);
+    }
+
+    @Test
+    public void testInputText() throws Exception {
+        // given
+        driver.get(baseUrl);
+
+        // when
+        driver.findElement(By.id("userUrlsList")).clear();
+        driver.findElement(By.id("userUrlsList")).sendKeys(TEXT);
+        driver.findElement(By.id("button")).click();
+        sleep(TIME_WAIT);
+
+        // then
+        String actualInputText = driver.findElement(By.id("ajaxResponse")).getText();
+        assertEquals(EXPECTED_INPUT_TEXT, actualInputText);
+    }
+
     @After
     public void tearDown() throws Exception {
         driver.quit();
@@ -140,7 +228,37 @@ public class WebFormTest {
         }
     }
 
-    private static final String EXPECTED_SORTING_KEY_ASCENDING = "Show\n" +
+    private static final String EXPECTED_INPUT_TEXT = "Show\n" +
+            "10\n" +
+            "25\n" +
+            "50\n" +
+            "100\n" +
+            "entries\n" +
+            "Search:\n" +
+            "Word Count\n" +
+            "one 4\n" +
+            "ёлка 3\n" +
+            "two 3\n" +
+            "білка 3\n" +
+            "объем 3\n" +
+            "їжак 2\n" +
+            "объём 1\n" +
+            "дом 1\n" +
+            "друг 1\n" +
+            "єнот 1\n" +
+            "Showing 1 to 10 of 18 entries\n" +
+            "Previous\n" +
+            "1\n" +
+            "2\n" +
+            "Next";
+
+    private static final String TEXT = "a One, the one ONE oNE  Two  two, two!@#$%^&*()_+=!123456789\n" +
+            "\n" +
+            "ёлка і Ёлка та ёлКА: ОБЪЁМ объем обЪем, але, но объем сказал завет человек время, имя, ученики, дом, друг, народ, слово, \n" +
+            "\n" +
+            "Їжак їжак єнот білка БІЛКА БіЛкА ";
+
+    private static final String EXPECTED_SHOW_ENTRIES = "Show\n" +
             "10\n" +
             "25\n" +
             "50\n" +
@@ -150,17 +268,118 @@ public class WebFormTest {
             "Word Count\n" +
             "one 4\n" +
             "two 3\n" +
-            "ааббввггддееёёжжззииййккллммннооппррссттууффххццччшшщщъъыыььээююяя 1\n" +
-            "ааббввггґґддееєєжжззииііїїййккллммннооппррссттууффххццччшшщщььююяя 1\n" +
             "білка 3\n" +
-            "объем 3\n" +
-            "объём 1\n" +
-            "ёлка 3\n" +
-            "єнот 1\n" +
             "їжак 2\n" +
-            "Showing 1 to 10 of 10 entries\n" +
+            "ёлка 2\n" +
+            "объем 2\n" +
+            "объём 1\n" +
+            "дом 1\n" +
+            "нообъем 1\n" +
+            "єнот 1\n" +
+            "время 1\n" +
+            "vkamenniygmailcom 1\n" +
+            "другнарод 1\n" +
+            "человек 1\n" +
+            "httphabrahabrrupoststopweekly 1\n" +
+            "ученики 1\n" +
+            "іёлка 1\n" +
+            "завет 1\n" +
+            "имя 1\n" +
+            "httpswwwgooglecomuasearchqjavapatterncompilesplitoqdbeddbcdpatterncompiledbdbdbddbaqschromeijljjsourceidchromeessmieutf- 1\n" +
+            "слово 1\n" +
+            "сказал 1\n" +
+            "Showing 1 to 22 of 22 entries\n" +
             "Previous\n" +
             "1\n" +
+            "Next";
+
+    private static final String EXPECTED_PREVIOUS_RESPONSE = "Show\n" +
+            "10\n" +
+            "25\n" +
+            "50\n" +
+            "100\n" +
+            "entries\n" +
+            "Search:\n" +
+            "Word Count\n" +
+            "one 4\n" +
+            "two 3\n" +
+            "білка 3\n" +
+            "їжак 2\n" +
+            "ёлка 2\n" +
+            "объем 2\n" +
+            "объём 1\n" +
+            "дом 1\n" +
+            "нообъем 1\n" +
+            "єнот 1\n" +
+            "Showing 1 to 10 of 22 entries\n" +
+            "Previous\n" +
+            "1\n" +
+            "2\n" +
+            "3\n" +
+            "Next";
+
+    private static final String EXPECTED_NEXT_RESPONSE = "Show\n" +
+            "10\n" +
+            "25\n" +
+            "50\n" +
+            "100\n" +
+            "entries\n" +
+            "Search:\n" +
+            "Word Count\n" +
+            "время 1\n" +
+            "vkamenniygmailcom 1\n" +
+            "другнарод 1\n" +
+            "человек 1\n" +
+            "httphabrahabrrupoststopweekly 1\n" +
+            "ученики 1\n" +
+            "іёлка 1\n" +
+            "завет 1\n" +
+            "имя 1\n" +
+            "httpswwwgooglecomuasearchqjavapatterncompilesplitoqdbeddbcdpatterncompiledbdbdbddbaqschromeijljjsourceidchromeessmieutf- 1\n" +
+            "Showing 11 to 20 of 22 entries\n" +
+            "Previous\n" +
+            "1\n" +
+            "2\n" +
+            "3\n" +
+            "Next";
+
+    private static final String EXPECTED_SEARCH_WORD = "Show\n" +
+            "10\n" +
+            "25\n" +
+            "50\n" +
+            "100\n" +
+            "entries\n" +
+            "Search:\n" +
+            "Word Count\n" +
+            "білка 3\n" +
+            "Showing 1 to 1 of 1 entries (filtered from 22 total entries)\n" +
+            "Previous\n" +
+            "1\n" +
+            "Next";
+
+    private static final String EXPECTED_SORTING_KEY_ASCENDING = "Show\n" +
+            "10\n" +
+            "25\n" +
+            "50\n" +
+            "100\n" +
+            "entries\n" +
+            "Search:\n" +
+            "Word Count\n" +
+            "httphabrahabrrupoststopweekly 1\n" +
+            "httpswwwgooglecomuasearchqjavapatterncompilesplitoqdbeddbcdpatterncompiledbdbdbddbaqschromeijljjsourceidchromeessmieutf- 1\n" +
+            "one 4\n" +
+            "two 3\n" +
+            "vkamenniygmailcom 1\n" +
+            "білка 3\n" +
+            "время 1\n" +
+            "дом 1\n" +
+            "другнарод 1\n" +
+            "завет 1\n" +
+            "Showing 1 to 10 of 22 entries\n" +
+            "Previous\n" +
+            "1\n" +
+            "2\n" +
+            "3\n" +
             "Next";
 
     private static final String EXPECTED_SORTING_VALUE_ASCENDING = "Show\n" +
@@ -172,18 +391,20 @@ public class WebFormTest {
             "Search:\n" +
             "Word Count\n" +
             "объём 1\n" +
-            "ааббввггґґддееєєжжззииііїїййккллммннооппррссттууффххццччшшщщььююяя 1\n" +
-            "ааббввггддееёёжжззииййккллммннооппррссттууффххццччшшщщъъыыььээююяя 1\n" +
+            "дом 1\n" +
+            "нообъем 1\n" +
             "єнот 1\n" +
-            "їжак 2\n" +
-            "білка 3\n" +
-            "ёлка 3\n" +
-            "two 3\n" +
-            "объем 3\n" +
-            "one 4\n" +
-            "Showing 1 to 10 of 10 entries\n" +
+            "время 1\n" +
+            "vkamenniygmailcom 1\n" +
+            "другнарод 1\n" +
+            "человек 1\n" +
+            "httphabrahabrrupoststopweekly 1\n" +
+            "ученики 1\n" +
+            "Showing 1 to 10 of 22 entries\n" +
             "Previous\n" +
             "1\n" +
+            "2\n" +
+            "3\n" +
             "Next";
 
     private static final String EXPECTED_SORTING_KEY_DESCENDING = "Show\n" +
@@ -194,19 +415,21 @@ public class WebFormTest {
             "entries\n" +
             "Search:\n" +
             "Word Count\n" +
-            "ааббввггддееёёжжззииййккллммннооппррссттууффххццччшшщщъъыыььээююяя 1\n" +
-            "ааббввггґґддееєєжжззииііїїййккллммннооппррссттууффххццччшшщщььююяя 1\n" +
+            "httphabrahabrrupoststopweekly 1\n" +
+            "httpswwwgooglecomuasearchqjavapatterncompilesplitoqdbeddbcdpatterncompiledbdbdbddbaqschromeijljjsourceidchromeessmieutf- 1\n" +
+            "vkamenniygmailcom 1\n" +
+            "время 1\n" +
+            "дом 1\n" +
+            "другнарод 1\n" +
+            "завет 1\n" +
+            "имя 1\n" +
+            "нообъем 1\n" +
             "объём 1\n" +
-            "єнот 1\n" +
-            "їжак 2\n" +
-            "two 3\n" +
-            "білка 3\n" +
-            "объем 3\n" +
-            "ёлка 3\n" +
-            "one 4\n" +
-            "Showing 1 to 10 of 10 entries\n" +
+            "Showing 1 to 10 of 22 entries\n" +
             "Previous\n" +
             "1\n" +
+            "2\n" +
+            "3\n" +
             "Next";
 
     private static final String EXPECTED_SORTING_VALUE_DESCENDING = "Show\n" +
@@ -218,17 +441,19 @@ public class WebFormTest {
             "Search:\n" +
             "Word Count\n" +
             "one 4\n" +
-            "білка 3\n" +
-            "ёлка 3\n" +
             "two 3\n" +
-            "объем 3\n" +
+            "білка 3\n" +
             "їжак 2\n" +
+            "ёлка 2\n" +
+            "объем 2\n" +
             "объём 1\n" +
-            "ааббввггґґддееєєжжззииііїїййккллммннооппррссттууффххццччшшщщььююяя 1\n" +
-            "ааббввггддееёёжжззииййккллммннооппррссттууффххццччшшщщъъыыььээююяя 1\n" +
+            "дом 1\n" +
+            "нообъем 1\n" +
             "єнот 1\n" +
-            "Showing 1 to 10 of 10 entries\n" +
+            "Showing 1 to 10 of 22 entries\n" +
             "Previous\n" +
             "1\n" +
+            "2\n" +
+            "3\n" +
             "Next";
 }
