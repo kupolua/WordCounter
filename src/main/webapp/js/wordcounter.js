1c7bb0631860802833e4f5757942b1cc179d96a8
kupol <pkulakovsky@gmail.com>
2014-09-13 08:45:53 +0300

    WORDS-224 - Spinner bind to inputForm

--- src/main/webapp/js/wordcounter.js
+++ src/main/webapp/js/wordcounter.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+    var opts = {
+        lines: 11, // The number of lines to draw
+        length: 17, // The length of each line
+        width: 10, // The line thickness
+        radius: 18, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 7, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#2ba6cb', // #rgb or #rrggbb or array of colors
+        speed: 1.1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '150', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    var target = document.getElementById('spinner');
+    var spinner;
+
 //Stops the submit request
     $("#myAjaxRequestForm").submit(function(e){
         e.preventDefault();
@@ -7,27 +28,6 @@ $(document).ready(function() {
 
     //checks for the button click event
     $("#button").click(function(e){
-
-        var opts = {
-            lines: 11, // The number of lines to draw
-            length: 17, // The length of each line
-            width: 10, // The line thickness
-            radius: 18, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 7, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#000', // #rgb or #rrggbb or array of colors
-            speed: 1.1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: '50%', // Top position relative to parent
-            left: '50%' // Left position relative to parent
-        };
-        var target = document.getElementById('spinner');
-        var spinner;
 
         //getTextType the form data and then serialize that
         dataString = $("#myAjaxRequestForm").serialize();
@@ -88,8 +88,8 @@ $(document).ready(function() {
                 //adding some Dummy data to the request
                 settings.data += "&dummyData=whatever";
                 //disable the button until we getTextType the response
-                spinner = new Spinner(opts).spin(target);
                 $('#button').attr("disabled", true);
+                spinner = new Spinner(opts).spin(target);
 
 
             },
