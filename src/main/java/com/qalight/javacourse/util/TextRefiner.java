cdb7b50813bffd52485a42e02491aca0f66e8870
Alexander Gandzha <agapeteo@gmail.com>
2014-09-14 03:09:13 -0400

    - code refactoring and todos

--- src/main/java/com/qalight/javacourse/util/TextRefiner.java
+++ src/main/java/com/qalight/javacourse/util/TextRefiner.java
@@ -3,16 +3,20 @@ package com.qalight.javacourse.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
 public class TextRefiner {
+    public static final Pattern WHITESPACES_PATTERN =
+            // todo: use constants with names for values like "&#160;" as reader do not understand what it means
+            Pattern.compile("(\\s+)|(&nbsp;)|(&#160;)|(&ensp;)|(&#8194;)|(&emsp;)|(&#8195;)|(&thinsp;)|(&#8201;)|(&zwnj;)|(&#8204;)|—");
     private static final Logger LOG = LoggerFactory.getLogger(TextRefiner.class);
     private static final String NON_BREAKING_HYPHEN = "&#8";
-    private static final Pattern WHITESPACES_PATTERN =
-            Pattern.compile("(\\s+)|(&nbsp;)|(&#160;)|(&ensp;)|(&#8194;)|(&emsp;)|(&#8195;)|(&thinsp;)|(&#8201;)|(&zwnj;)|(&#8204;)|—");
     private static final Pattern HYPHEN_PATTERN = Pattern.compile("(.+-)|(-.+)");
     private static final Pattern URL_PATTERN = Pattern.compile("(http://.*)|(https://.*)|(ftp://.*)|(www\\..*)");
     private static final Pattern CLEAN_PATTERN = Pattern.compile("[^a-zA-Zа-яА-Я-іІїЇєЄёЁґҐ]");
@@ -20,7 +24,8 @@ public class TextRefiner {
     public List<String> refineText(String unrefinedPlainText) {
         checkIfPlainTextIsNullOrEmpty(unrefinedPlainText);
 
-        List<String> words = new ArrayList<>(Arrays.asList(WHITESPACES_PATTERN.split(unrefinedPlainText)));
+        List<String> unrefinedWords = asSplitList(unrefinedPlainText);
+        List<String> words = new ArrayList<>(unrefinedWords);
 
         List<String> urlsList = new ArrayList<>();
         for (String word : words) {
@@ -30,12 +35,13 @@ public class TextRefiner {
             }
         }
         words.removeAll(urlsList);
-
         words = cleanWords(words);
-
         words.addAll(urlsList);
-
         return words;
+    }
+
+    public static List<String> asSplitList(String unrefinedPlainText) {
+        return Arrays.asList(WHITESPACES_PATTERN.split(unrefinedPlainText));
     }
 
     private List<String> cleanWords(List<String> words) {
--- src/main/java/com/qalight/javacourse/util/UrlFixer.java
+++ src/main/java/com/qalight/javacourse/util/UrlFixer.java
@@ -3,6 +3,7 @@ package com.qalight.javacourse.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -15,11 +16,13 @@ public class UrlFixer {
     private static final int TIMEOUT = 1000;
     private static final String HTTP_PREFIX = "http://";
     private static final String HTTPS_PREFIX = "https://";
+    private static final int HTTP_SUCCESS_CODE = 200;
+    private static final int HTTP_USER_ERROR_CODE = 400;
 
     public String fixRequest(String userRequest){
         String request;
-        String fixedUrl = fixUrl(userRequest);
-        if(checkUrl(fixedUrl, TIMEOUT)){
+        String fixedUrl = addHttpProtocolPrefixIfNotExist(userRequest);
+        if(checkUrlAlive(fixedUrl, TIMEOUT)){
             request = fixedUrl;
         } else {
             request = userRequest;
@@ -27,27 +30,37 @@ public class UrlFixer {
         return request;
     }
 
-    private String fixUrl(String userUrl) {
+    private String addHttpProtocolPrefixIfNotExist(String userUrl) {
         String sourcesWithoutWhitespaces = userUrl.trim();
-        if (!(sourcesWithoutWhitespaces.startsWith(HTTPS_PREFIX) || sourcesWithoutWhitespaces.startsWith(HTTP_PREFIX))) {
+
+        boolean isHttpOrHttps = sourcesWithoutWhitespaces.startsWith(HTTPS_PREFIX)
+                || sourcesWithoutWhitespaces.startsWith(HTTP_PREFIX);
+
+        if (!isHttpOrHttps) {
             LOG.debug("Try to fix URL: " + sourcesWithoutWhitespaces);
             sourcesWithoutWhitespaces = HTTP_PREFIX + sourcesWithoutWhitespaces;
         }
         return sourcesWithoutWhitespaces;
     }
 
-    private boolean checkUrl(String url, int timeout) {
+    public boolean checkUrlAlive(String url, int timeout) {
         url = url.replaceFirst("https", "http");
 
+        HttpURLConnection connection = null;
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setConnectTimeout(timeout);
             connection.setReadTimeout(timeout);
             connection.setRequestMethod("HEAD");
             int responseCode = connection.getResponseCode();
-            return (200 <= responseCode && responseCode <= 399);
-        } catch (Exception e) {
+            boolean okReturnCode = HTTP_SUCCESS_CODE <= responseCode && responseCode < HTTP_USER_ERROR_CODE;
+            return okReturnCode;
+        } catch (Throwable e) {
             return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 }
