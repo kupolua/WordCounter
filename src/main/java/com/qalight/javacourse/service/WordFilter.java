b029d71d32f9ea9068f602ce91cd077160ff00a6
diverfd <diverfd@gmail.com>
2014-09-20 16:55:31 +0300

    WORDS-196, WORDS-222, WORDS-223

--- src/main/java/com/qalight/javacourse/service/WordFilter.java
+++ src/main/java/com/qalight/javacourse/service/WordFilter.java
@@ -1,50 +1,33 @@
 package com.qalight.javacourse.service;
 
-import com.qalight.javacourse.util.TextRefiner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import java.io.*;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 @Component
 public class WordFilter {
-    private String keyWordsForFilterEN = "wordsEN";
-    private String keyWordsForFilterRU = "wordsRU";
-    private String keyWordsForFilterUA = "wordsUA";
+    @Value("${wordsEN}")
+    private String wordsEn;
+    @Value("${wordsRU}")
+    private String wordsRU;
+    @Value("${wordsUA}")
+    private String wordsUA;
 
     public List<String> removeUnimportantWords(List<String> refinedWords) {
-        String wordsForFilter = getPropertiesValue(keyWordsForFilterEN) + " " + getPropertiesValue(keyWordsForFilterRU)
-                + " " + getPropertiesValue(keyWordsForFilterUA);
-        TextRefiner textRefiner = new TextRefiner();
-        List<String> filter = textRefiner.refineText(wordsForFilter);
+        String wordsForFilter = getWordsForFilter();
+        List<String> filter = Arrays.asList(wordsForFilter.split(" "));
         refinedWords.removeAll(filter);
         return refinedWords;
     }
 
-    public String getPropertiesValue(String key) {
-        String propFileName = "wordcounter.properties";
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-        try {
-            Reader reader = new InputStreamReader(inputStream, "UTF-8");
-            try {
-                properties.load(reader);
-            } finally {
-                reader.close();
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        String value = properties.getProperty(key);
-        return value;
+    public String getWordsForFilter() {
+        StringBuilder wordsForFilter = new StringBuilder();
+        wordsForFilter.append(wordsEn);
+        wordsForFilter.append(" ");
+        wordsForFilter.append(wordsRU);
+        wordsForFilter.append(" ");
+        wordsForFilter.append(wordsUA);
+        return wordsForFilter.toString();
     }
 }
