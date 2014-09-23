66b7f102fc83dbfd068bdd3efb038f665998ba95
diverfd <diverfd@gmail.com>
2014-09-23 15:17:59 +0300

    WORDS-243, Create Unit-test for WordFilter

--- src/test/java/com/qalight/javacourse/service/WordFilterTest.java
+++ src/test/java/com/qalight/javacourse/service/WordFilterTest.java
@@ -12,7 +12,7 @@ public class WordFilterTest {
     @Test
     public void removeUnimportantWords_removeOneWordEng() {
         // given
-        final WordFilter filter = new WordFilter("the ", "", "");
+        final WordFilter filter = new WordFilter("the", "", "");
         final List<String> refinedWords = new ArrayList<>(Arrays.asList("world", "the", "love"));
 
         // when
@@ -23,5 +23,74 @@ public class WordFilterTest {
         Assert.assertArrayEquals(expectedResult, actualResult.toArray());
     }
 
-    // todo: add other tests
+    @Test
+    public void removeUnimportantWords_removeOneWordRu() {
+        // given
+        final WordFilter filter = new WordFilter("", "и", "");
+        final List<String> refinedWords = new ArrayList<>(Arrays.asList("мир", "и", "любовь"));
+
+        // when
+        final List<String> actualResult = filter.removeUnimportantWords(refinedWords);
+
+        // then
+        String[] expectedResult = {"мир", "любовь"};
+        Assert.assertArrayEquals(expectedResult, actualResult.toArray());
+    }
+
+    @Test
+    public void removeUnimportantWords_removeOneWordUa() {
+        // given
+        final WordFilter filter = new WordFilter("", "", "і");
+        final List<String> refinedWords = new ArrayList<>(Arrays.asList("світ", "і", "любов"));
+
+        // when
+        final List<String> actualResult = filter.removeUnimportantWords(refinedWords);
+
+        // then
+        String[] expectedResult = {"світ", "любов"};
+        Assert.assertArrayEquals(expectedResult, actualResult.toArray());
+    }
+
+    @Test
+    public void removeUnimportantWords_removeWordsInDifferentLanguages() {
+        // given
+        final WordFilter filter = new WordFilter("the", "и", "і");
+        final List<String> refinedWords = new ArrayList<>(Arrays.asList("світ", "і", "любов", "мир", "и", "любовь", "world", "the", "love"));
+
+        // when
+        final List<String> actualResult = filter.removeUnimportantWords(refinedWords);
+
+        // then
+        String[] expectedResult = {"світ", "любов", "мир", "любовь", "world", "love"};
+        Assert.assertArrayEquals(expectedResult, actualResult.toArray());
+    }
+
+    @Test
+    public void removeUnimportantWords_removeVarietyWordsInDifferentLanguages() {
+        // given
+        final WordFilter filter = new WordFilter("the your they with", "и что его на", "і він їх лише");
+        final List<String> refinedWords = new ArrayList<>(Arrays.asList
+                ("світ", "і", "любов", "мир", "и", "любовь", "world", "the", "love", "він", "їх", "лише", "что", "его", "на", "your", "they", "with"));
+
+        // when
+        final List<String> actualResult = filter.removeUnimportantWords(refinedWords);
+
+        // then
+        String[] expectedResult = {"світ", "любов", "мир", "любовь", "world", "love"};
+        Assert.assertArrayEquals(expectedResult, actualResult.toArray());
+    }
+
+    @Test
+    public void removeUnimportantWords_removeWordsFromCleanRefinedList() {
+        // given
+        final WordFilter filter = new WordFilter("the your they with", "и что его на", "і він їх лише");
+        final List<String> refinedWords = new ArrayList<>(Arrays.asList("світ", "любов", "мир", "любовь", "world", "love"));
+
+        // when
+        final List<String> actualResult = filter.removeUnimportantWords(refinedWords);
+
+        // then
+        String[] expectedResult = {"світ", "любов", "мир", "любовь", "world", "love"};
+        Assert.assertArrayEquals(expectedResult, actualResult.toArray());
+    }
 }
