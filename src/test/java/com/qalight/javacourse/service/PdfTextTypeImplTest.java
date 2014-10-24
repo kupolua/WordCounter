4ef606bd1bc38552e4b6db5af3d4af43d9c71b2f
StKotok <stkotok@gmail.com>
2014-10-24 21:07:07 +0400

    WORDS-354: create unit tests for PdfTextTypeImpl.java class

---
+++ src/test/java/com/qalight/javacourse/service/PdfTextTypeImplTest.java
@@ -0,0 +1,21 @@
+package com.qalight.javacourse.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PdfTextTypeImplTest {
+    PdfTextTypeImpl pdfTextType;
+
+    @Before
+    public void setUp() throws Exception {
+        pdfTextType = new PdfTextTypeImpl();
+    }
+
+    @Test
+    public void testIsEligible() throws Exception {
+
+
+    }
+}
