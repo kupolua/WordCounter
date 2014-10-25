4d815fa9483d677c1816fe67cced87554125a9dd
StKotok <stkotok@gmail.com>
2014-10-25 21:50:39 +0400

    WORDS-357: refine tests for PlainToStringConverter.java class

--- src/main/java/com/qalight/javacourse/service/PlainToStringConverter.java
+++ src/main/java/com/qalight/javacourse/service/PlainToStringConverter.java
@@ -1,10 +1,6 @@
 package com.qalight.javacourse.service;
 
-/**
- * Created by box on 08.09.2014.
- */
 public class PlainToStringConverter implements DocumentToStringConverter {
-
     @Override
     public boolean isEligible(TextType documentType) {
         boolean isEligible = false;
