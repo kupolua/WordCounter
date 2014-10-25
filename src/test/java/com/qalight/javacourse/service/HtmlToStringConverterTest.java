1c0a30ebdfbc1ce0698ff5728d6cb2f14a362c30
diverfd <diverfd@gmail.com>
2014-10-25 17:15:27 +0300

    WORDS-352 - create tests for HtmlToStringConverter.java class, some refactoring

--- src/test/java/com/qalight/javacourse/service/HtmlToStringConverterTest.java
+++ src/test/java/com/qalight/javacourse/service/HtmlToStringConverterTest.java
@@ -1,12 +1,26 @@
 package com.qalight.javacourse.service;
 
+import static org.mockito.Mockito.*;
+
+import org.jsoup.nodes.Document;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
+
+@RunWith(MockitoJUnitRunner.class)
 public class HtmlToStringConverterTest {
-
     private HtmlToStringConverter converter;
+
+    @Mock
+    private Document document;
+    @Spy
+    private HtmlToStringConverter spyConverter;
 
     @Before
     public void setup() {
@@ -35,5 +49,39 @@ public class HtmlToStringConverterTest {
 
         //then
         Assert.assertFalse(actualResult);
+    }
+
+    @Test
+    public void convertToString() throws Exception{
+        //given
+        final String input = "http://pdf-examples.com/example.html";
+        final String expected = "some text";
+        doReturn(document).when(spyConverter).getDocument(anyString());
+        doReturn(expected).when(spyConverter).getPlainText(any(Document.class));
+
+        //when
+        final String actual = spyConverter.convertToString(input);
+
+        //then
+        verify(spyConverter, times(1)).getPlainText(any(Document.class));
+        verify(spyConverter, times(1)).getDocument(anyString());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void convertToString_throwingException() throws Exception{
+        //given
+        final String input = "http://pdf-examples.com/example.html";
+        final String expected = "some text";
+        doThrow(new IOException("test")).when(spyConverter).getDocument(anyString());
+        doReturn(expected).when(spyConverter).getPlainText(any(Document.class));
+
+        //when
+        final String actual = spyConverter.convertToString(input);
+
+        //then
+        verify(spyConverter, never()).getPlainText(any(Document.class));
+        verify(spyConverter, times(1)).getDocument(anyString());
+        Assert.assertEquals(expected, actual);
     }
 }
