0d036b12e409003dfe5e271eb25220c7cc5802d4
vkamenniy <vkamenniy@gmail.com>
2014-10-28 13:25:10 +0200

    WORDS-323 - Functional tests: Results presentation functionality. Choose a mode of display: 10, 25, 50, 100 words per page

--- src/functionalTest/java/com/qalight/javacourse/webForm/ResultsPresentationFunctionalityTest.java
+++ src/functionalTest/java/com/qalight/javacourse/webForm/ResultsPresentationFunctionalityTest.java
@@ -12,9 +12,12 @@ import static org.junit.Assert.assertEquals;
 import static com.qalight.javacourse.webForm.utils.Constants.*;
 import static com.qalight.javacourse.webForm.utils.Util.*;
 import static org.junit.Assert.fail;
-
+// todo: change test link (add words)
 public class ResultsPresentationFunctionalityTest {
     private static WebDriver driver;
+
+    private final String ELEMENT_DATA_TABLES_LENGTH = "countedWords_length";
+    private final String DATA_TABLES_LENGTH_100 = "100";
 
     @BeforeClass
     public static void init() {
@@ -57,7 +60,6 @@ public class ResultsPresentationFunctionalityTest {
 
         // then
         if (isReady) {
-            final String ELEMENT_DATA_TABLES_LENGTH = "countedWords_length";
             final String DATA_TABLES_LENGTH_25 = "25";
             new Select(driver.findElement(By.name(ELEMENT_DATA_TABLES_LENGTH))).selectByVisibleText(DATA_TABLES_LENGTH_25);
             final String EXPECTED_SHOW_ENTRIES_25 =
@@ -66,6 +68,77 @@ public class ResultsPresentationFunctionalityTest {
                     "время 1\n" + "та 1\n" + "the 1\n" + "человек 1\n" + "народ 1\n" + "r 1\n" + "завет 1\n" + "сказал 1";
             String actualShowEntries = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
             assertEquals(EXPECTED_SHOW_ENTRIES_25, actualShowEntries);
+        } else {
+            fail(RESPONSE_IS_NOT_READY);
+        }
+    }
+
+    @Test
+     public void testShowEntries50() throws Exception {
+        // given
+        driver.get(BASE_URL);
+
+        // when
+        putDataAndClickCountButton(driver, HTML_TEST_PAGE);
+
+        boolean isReady = waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
+
+        // then
+        if (isReady) {
+            final String DATA_TABLES_LENGTH_50 = "50";
+            new Select(driver.findElement(By.name(ELEMENT_DATA_TABLES_LENGTH))).selectByVisibleText(DATA_TABLES_LENGTH_50);
+            final String EXPECTED_SHOW_ENTRIES_50 =
+                    "one 4\n" + "ёлка 3\n" + "two 3\n" + "білка 3\n" + "объем 3\n" + "їжак 2\n" + "объём 1\n" + "ученики 1\n" +
+                            "і 1\n" + "але 1\n" + "имя 1\n" + "слово 1\n" + "a 1\n" + "но 1\n" + "дом 1\n" + "друг 1\n" + "єнот 1\n" +
+                            "время 1\n" + "та 1\n" + "the 1\n" + "человек 1\n" + "народ 1\n" + "r 1\n" + "завет 1\n" + "сказал 1";
+            String actualShowEntries = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
+            assertEquals(EXPECTED_SHOW_ENTRIES_50, actualShowEntries);
+        } else {
+            fail(RESPONSE_IS_NOT_READY);
+        }
+    }
+
+    @Test
+    public void testShowEntries100() throws Exception {
+        // given
+        driver.get(BASE_URL);
+
+        // when
+        putDataAndClickCountButton(driver, HTML_TEST_PAGE);
+
+        boolean isReady = waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
+
+        // then
+        if (isReady) {
+            new Select(driver.findElement(By.name(ELEMENT_DATA_TABLES_LENGTH))).selectByVisibleText(DATA_TABLES_LENGTH_100);
+            final String EXPECTED_SHOW_ENTRIES_100 =
+                    "one 4\n" + "ёлка 3\n" + "two 3\n" + "білка 3\n" + "объем 3\n" + "їжак 2\n" + "объём 1\n" + "ученики 1\n" +
+                            "і 1\n" + "але 1\n" + "имя 1\n" + "слово 1\n" + "a 1\n" + "но 1\n" + "дом 1\n" + "друг 1\n" + "єнот 1\n" +
+                            "время 1\n" + "та 1\n" + "the 1\n" + "человек 1\n" + "народ 1\n" + "r 1\n" + "завет 1\n" + "сказал 1";
+            String actualShowEntries = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
+            assertEquals(EXPECTED_SHOW_ENTRIES_100, actualShowEntries);
+        } else {
+            fail(RESPONSE_IS_NOT_READY);
+        }
+    }
+
+    @Test
+    public void testShowEntries10() throws Exception {
+        // given
+        driver.get(BASE_URL);
+
+        // when
+        putDataAndClickCountButton(driver, HTML_TEST_PAGE);
+
+        boolean isReady = waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
+
+        // then
+        if (isReady) {
+            final String DATA_TABLES_LENGTH_10 = "10";
+            new Select(driver.findElement(By.name(ELEMENT_DATA_TABLES_LENGTH))).selectByVisibleText(DATA_TABLES_LENGTH_100);
+            new Select(driver.findElement(By.name(ELEMENT_DATA_TABLES_LENGTH))).selectByVisibleText(DATA_TABLES_LENGTH_10);
+            String actualShowEntries = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
+            assertEquals(EXPECTED_STANDARD_RESULT, actualShowEntries);
         } else {
             fail(RESPONSE_IS_NOT_READY);
         }
