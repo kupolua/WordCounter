a84da7bdb10b7412f293f3fcd5b84c0d48cd9f94
Vladimir Kamenniy <vkamenniy@gmail.com>
2014-11-15 19:02:04 +0200

    WORDS-411 - Acceptance: Search a word in results table

--- src/acceptanceTest/java/com/qalight/javacourse/webForm/SearchingFunctionalityTest.java
+++ src/acceptanceTest/java/com/qalight/javacourse/webForm/SearchingFunctionalityTest.java
@@ -29,12 +29,53 @@ public class SearchingFunctionalityTest {
     public void testSearchWord() throws Exception {
         // given
         driver.get(BASE_URL);
+        final String htmlPageCyrillic = "http://kupol.in.ua/wordcounter/testData/page_cyrillic.html";
         final String elementCssInputSearch = "input[type=\"search\"]";
-        final String searchWord = "білка";
-        final String expectedSearchWord = "білка 3";
+        final String searchWord = "нет";
+        final String expectedSearchWord = "нет 1";
 
         // when
-        putDataAndClickCountButton(driver, HTML_TEST_PAGE);
+        putDataAndClickCountButton(driver, htmlPageCyrillic);
+        waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
+        driver.findElement(By.cssSelector(elementCssInputSearch)).clear();
+        driver.findElement(By.cssSelector(elementCssInputSearch)).sendKeys(searchWord);
+
+        // then
+        String actualSearchWord = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
+        assertEquals(expectedSearchWord, actualSearchWord);
+    }
+
+//    @Test
+    public void testSearchWord_noSuchWord() throws Exception {
+        // given
+        driver.get(BASE_URL);
+        final String htmlPageLatin = "http://kupol.in.ua/wordcounter/testData/page_latin.html";
+        final String elementCssInputSearch = "input[type=\"search\"]";
+        final String searchWord = "yet";
+        final String expectedSearchWord = "";
+
+        // when
+        putDataAndClickCountButton(driver, htmlPageLatin);
+        waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
+        driver.findElement(By.cssSelector(elementCssInputSearch)).clear();
+        driver.findElement(By.cssSelector(elementCssInputSearch)).sendKeys(searchWord);
+
+        // then
+        String actualSearchWord = driver.findElement(By.cssSelector(ANCHOR_HTML_PAGE_WITH_WORDS)).getText();
+        assertEquals(expectedSearchWord, actualSearchWord);
+    }
+
+    @Test
+    public void testSearchWord_putNumber() throws Exception {
+        // given
+        driver.get(BASE_URL);
+        final String odtLink = "http://kupol.in.ua/wordcounter/testData/all_lang.odt";
+        final String elementCssInputSearch = "input[type=\"search\"]";
+        final String searchWord = "2";
+        final String expectedSearchWord = "аэросъемка 2\n" + "sweet 2\n" + "дымарь 2";
+
+        // when
+        putDataAndClickCountButton(driver, odtLink);
         waitForJQueryProcessing(driver, WAIT_FOR_ELEMENT);
         driver.findElement(By.cssSelector(elementCssInputSearch)).clear();
         driver.findElement(By.cssSelector(elementCssInputSearch)).sendKeys(searchWord);
