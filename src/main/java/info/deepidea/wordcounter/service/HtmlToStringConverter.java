11fd2802d3bb8da204885225b28ab2f3d101bbe5
diverfd <diverfd@gmail.com>
2015-02-24 14:44:20 +0200

    WORDS-509 - fix "Pravda" issue

--- src/main/java/info/deepidea/wordcounter/service/HtmlToStringConverter.java
+++ src/main/java/info/deepidea/wordcounter/service/HtmlToStringConverter.java
@@ -3,6 +3,7 @@ package info.deepidea.wordcounter.service;
 import info.deepidea.wordcounter.util.Assertions;
 import info.deepidea.wordcounter.util.ErrorCodeImpl;
 import info.deepidea.wordcounter.util.WordCounterRuntimeException;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.examples.HtmlToPlainText;
 import org.jsoup.nodes.Document;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 public class HtmlToStringConverter implements DocumentToStringConverter {
@@ -51,9 +53,14 @@ public class HtmlToStringConverter implements DocumentToStringConverter {
 
     protected Document getDocument(String userUrl) throws IOException {
         final int timeout = 3000;
+        Connection.Response con = Jsoup.connect(userUrl)
+                .timeout(timeout)
+                .userAgent(USER_AGENT_VALUE).execute();
+        Map<String, String> cookies = con.cookies();
         Document doc = Jsoup.connect(userUrl)
                 .timeout(timeout)
                 .userAgent(USER_AGENT_VALUE)
+                .cookies(cookies)
                 .get();
         return doc;
     }
